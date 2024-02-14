a3faa599c028ad0582a658541f37808d58470b09
SpaceCowboy <fadeev115@gmail.com>
2024-02-14 20:43:11 +0300

    add precode for test in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+
+        Epic epic = new Epic("EpicTask", "EpicTaskDescr");
+        SubTask subTask = new SubTask("subTask", "subTaskDescr", Progress.NEW);
+        SubTask subTask1 = new SubTask("subTask1", "anotherDescr", Progress.NEW);
+        SubTask subTask2 = new SubTask("StandAloneTask", "someDescr", Progress.NEW);
+
+
+        TaskManager manager = new TaskManager();
+        manager.addTask(epic);
+        manager.addTask(subTask);
+        manager.addTask(subTask1);
+        manager.addTask(subTask2);
+
+        manager.addSubtaskToEpic(subTask, epic);
+        manager.addSubtaskToEpic(subTask1, epic);
+
+        /*
+        METHODS TO TEST
+
+        addTask (Task task)
+        addSubtaskToEpic(SubTask subTask, Epic epic)
+        getTasks()
+        getEpicTasks()
+        getSubTasks()
+        updateTask (SubTask oldTask, SubTask newTask)
+        updateEpicTask (Epic oldEpic, Epic newEpic)
+        removeAllTasks()
+        removeTaskById(int id)
+        removeEpicTaskById(int id)
+        getTaskById(int id)
+         */
+
     }
 }
